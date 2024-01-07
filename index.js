e0526012af940ae95422a211311a5e8629dd191c
RitaM5 <sporshia108514orva@gmail.com>
2024-01-07 23:20:16 +0600

    food cart site added

---
+++ index.js
@@ -0,0 +1,171 @@
+let listProductHTML = document.querySelector('.listProduct');
+let listCartHTML = document.querySelector('.listCart');
+let iconCart = document.querySelector('.icon-cart');
+let iconCartSpan = document.querySelector('.icon-cart span');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let closeCart = document.querySelector('.close');
+let products = [];
+let cart = [];
+
+
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+})
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+})
+
+const addDataToHTML = () => {
+    // remove datas default from HTML
+
+    // add new datas
+    if (products.length > 0) // if has data
+    {
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.dataset.id = product.id;
+            newProduct.classList.add('item');
+            newProduct.innerHTML =
+                `<img class="h-50 w-" src="${product.image}" alt="">       
+                <h2 class="mt-4">${product.name}</h2>
+                <div class="price">$${product.price}</div>
+                <p>${product.details}</p>
+                <button class="addCart">Add To Cart</button>`;
+            listProductHTML.appendChild(newProduct);
+            let addButton = newProduct.querySelector('.addCart');
+            addButton.addEventListener('click', () => {
+                addToCart(product.id);
+                addButton.disabled = true; // Disable the button after it's clicked
+            });
+        });
+    }
+}
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if (positionClick.classList.contains('addCart')){
+        let id_product = positionClick.parentElement.dataset.id;
+        addToCart(id_product);
+    }
+})
+const addToCart = (product_id) => {
+    let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
+    if (cart.length <= 0) {
+        cart = [{
+            product_id: product_id,
+            quantity: 1
+        }];
+    } else if (positionThisProductInCart < 0) {
+        cart.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    } else {
+        cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    if (cart.length > 0) {
+        cart.forEach(item => {
+            totalQuantity = totalQuantity + item.quantity;
+            let newItem = document.createElement('div');
+            newItem.classList.add('item');
+            newItem.dataset.id = item.product_id;
+
+            let positionProduct = products.findIndex((value) => value.id == item.product_id);
+            let info = products[positionProduct];
+           totalPrice = totalPrice + info.price * item.quantity ;
+            listCartHTML.appendChild(newItem);
+            newItem.innerHTML = `
+            <div class="image">
+                    <img src="${info.image}">
+                </div>
+                <div class="name">
+                ${info.name}
+                </div>
+                <div class="totalPrice">$${info.price * item.quantity}</div>
+                <div class="quantity">
+                    <span class="minus"><</span>
+                    <span>${item.quantity}</span>
+                    <span class="plus">></span>
+                </div>
+                <div class="">
+                <button class="delete border-0 my-2">Delete</button>
+                </div>
+            `;
+        })
+        total.innerText = totalPrice;
+    }
+    iconCartSpan.innerText = totalQuantity;
+}
+
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if (positionClick.classList.contains('minus') || positionClick.classList.contains('plus')) {
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'minus';
+        if (positionClick.classList.contains('plus')) {
+            type = 'plus';
+        }
+        changeQuantityCart(product_id, type);
+    }
+    else if (positionClick.classList.contains('delete')) {
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        deleteCartItem(product_id);
+    }
+})
+const changeQuantityCart = (product_id, type) => {
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if (positionItemInCart >= 0) {
+        let info = cart[positionItemInCart];
+        switch (type) {
+            case 'plus':
+                cart[positionItemInCart].quantity = cart[positionItemInCart].quantity + 1;
+                break;
+
+            default:
+                let changeQuantity = cart[positionItemInCart].quantity - 1;
+                if (changeQuantity > 0) {
+                    cart[positionItemInCart].quantity = changeQuantity;
+                } else {
+                    cart.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+const deleteCartItem = (product_id) => {
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if (positionItemInCart >= 0) {
+        cart.splice(positionItemInCart, 1);
+        addCartToHTML();
+        addCartToMemory();
+    }
+};
+
+const initApp = () => {
+    // get data product
+    fetch('products.json')
+        .then(response => response.json())
+        .then(data => {
+            products = data;
+            addDataToHTML();
+
+            // get data cart from memory
+            if (localStorage.getItem('cart')) {
+                cart = JSON.parse(localStorage.getItem('cart'));
+                addCartToHTML();
+            }
+        })
+}
+initApp();
